5be3366e990599a901bfaed4866794eef7faa8cd
Dong Hur <dhur@college.harvard.edu>
2018-05-17 21:11:49 -0500

    WIP Degree 2 Smaller Network Shortest path

---
+++ SmallerNetwork/GeneNetworkAnalysis.m
@@ -0,0 +1,19 @@
+%% Loads interested 21 genes Corr Matrix
+clc; clear all;
+load ADGeneticRiskNetwork_Allen.mat;
+
+%% Deletes nodes with only one degree (only one edge)
+% VARIABLES
+degree_threshold = 2;
+
+% ANALYSIS
+C_Matrix=AllenBrain_FreesurferROIs_corr_ADGeneticRiskNetwork;
+C_Matrix_bin=C_Matrix>0; 
+% finds the number of degrees for each node
+sum_BinMatrix=sum(C_Matrix_bin);
+% filter out nodes with only one degree
+idx_InterestedNode = find(sum_BinMatrix>=degree_threshold);
+% include only interested 
+C_Matrix=C_Matrix(:,idx_InterestedNode);
+%% Convert Corr Matrix ==> .gexf
+WriteCorrToGEXF(C_Matrix, AllenBrain_genesnames, ADgenes_AllenBrain_genesnames_location, 'Network_2Deg')
